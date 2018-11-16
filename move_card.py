d6687fa073f162dbe1b169240688638307b8c228
shinonome128 <shinonome128@gmail.com>
2018-11-17 04:35:34 +0900

    Add card movement process

--- move_card.py
+++ move_card.py
@@ -28,6 +28,7 @@ def main():
             OAUTH_TOKEN_SECRET = config.get('API', 'OAUTH_TOKEN_SECRET')
             BOARD_NAME = config.get('ENV', 'BOARD_NAME')
             TAG_NAME = config.get('ENV', 'TAG_NAME')
+            DST_LIST_NAME = config.get('ENV', 'DST_LIST_NAME')
 
         # 失敗した時はエラーとだけ伝える
         except:
@@ -40,27 +41,11 @@ def main():
         # 緑ラベルカードの配列を呼び出し
         green_cards = get_green_cards(client, BOARD_NAME, TAG_NAME)
 
-        # クリップボードにコピー
-        copy_clipboard(green_cards)
+        # 宛先リスト ID を取得
+        dst_list_id = get_dst_list_id(client, BOARD_NAME, DST_LIST_NAME)
 
-"""
-カードリストの整形・クリップボードへ格納
-"""
-def copy_clipboard(cards_list):
-
-        # 空の文字列を作成
-        line = ''
-
-        # カードを取り出してループ処理
-        for card in cards_list:
-
-            # カード名と改行を格納
-            line += card.name + '\n'
-
-        # クリップボードにコピー
-        pyperclip.copy(line)
-
-        return
+        # 緑ラベルカードを宛先リストへ移動
+        move_cards(green_cards, dst_list_id)
 
 """
 緑ラベルカードの配列を取得
@@ -95,6 +80,44 @@ def get_green_cards(client, BOARD_NAME, TAG_NAME):
                                 green_cards.append(card)
 
         return green_cards
+
+"""
+宛先リストの ID を取得
+"""
+def get_dst_list_id(client, BOARD_NAME, DST_LIST_NAME):
+
+        # ボードリストを取得
+        board_list = client.list_boards()
+
+        # ボード名を取得してループ処理
+        for board in board_list:
+
+            # ボード名が一致するものを処理
+            if board.name == BOARD_NAME:
+
+                # リスト名を取得してループ処理
+                for list in board.list_lists('open'):
+
+                    # リスト名が一致するものを処理
+                    if list.name == DST_LIST_NAME:
+
+                        # リスト ID を取得
+                        dst_list_id = list.id
+
+        return dst_list_id
+
+"""
+カードリストを宛先リストに移動
+"""
+def move_cards(green_cards, dst_list_id):
+
+        # カード名を取得してループ処理
+        for card in green_cards:
+
+            # カードを宛先リストに移動
+            card.change_list(dst_list_id)
+
+        return
 
 """
 お作法、他ファイルから呼び出された場合は、このスクリプトは実行されない
